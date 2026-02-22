166a0ffc3aa9f96b0a39cc2ac8b1b88f51ba9c34
Mahesh Babu Neeli <maheshbabu22.neeli@gmail.com>
2026-02-22 13:13:50 +1100

    Terraform: Reading variables to create infrastructure

--- README.md
+++ README.md
@@ -7,3 +7,17 @@
 - etc.,. can connect to many tools
 
 - AWS Provider developed by both AWS and Terraform
+
+#### Commands
+- `terraform init` -> Download the provider
+- `terraform plan` -> Plan the infrastructure, but it will not create
+- `terraform apply` -> Create the infrastructure
+- `terraform destroy` -> Destroy the infrastructure
+
+#### Variables Types
+- string
+- number
+- map
+- list
+- set
+- bool
---
+++ variables/variables.tf
@@ -0,0 +1,51 @@
+variable "ami_id" {
+  type = string
+  description = "AMI ID"
+  default = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_type" {
+  type = string
+  description = "Instance type of the EC2 instance"
+  default = "t3.micro"
+}
+
+variable "ec2_tags" {
+  type = map(string)
+  default = {
+    Name = "terraform"
+    Project = "roboshop"
+    Terrrform = "true"
+    Environment = "dev"
+  }
+}
+
+variable "sg_name" {
+  type = string
+  default = "allow-all-terraform"
+}
+
+variable "sg_from_port" {
+  type = number
+  default = 0
+}
+
+variable "sg_to_port" {
+  type = number
+  default = 0
+}
+
+variable "sg_cider_blocks" {
+  type = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "sg_tags" {
+  type = map(string)
+  default = {
+    Name = "terraform"
+    Project = "roboshop"
+    Terrrform = "true"
+    Environment = "dev"
+  }
+}
